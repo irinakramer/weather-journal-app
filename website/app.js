90c68fb725a29a3f01bd89fe4b94a2ae9b0f0ef4
Irina Kramer <irakramer@gmail.com>
2020-10-20 14:19:13 -0700

    moved require to app

--- website/app.js
+++ website/app.js
@@ -17,6 +17,9 @@
  * 
 */
 
+// require env var
+require('dotenv').config();
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
